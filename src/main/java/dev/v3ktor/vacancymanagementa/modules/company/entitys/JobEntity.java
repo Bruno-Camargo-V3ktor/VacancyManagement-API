73d59b275a1725013bcd078d5629f6c3959f2770
Bruno C. <v3ktoryt@gmail.com>
2024-09-05 21:52:56 -0300

    ✨ feat: 'Criação da classe base da entidade Job(Vaga) com relacionamento de MuitosParaUm com a entidade Company'

---
+++ src/main/java/dev/v3ktor/vacancymanagementa/modules/company/entitys/JobEntity.java
@@ -0,0 +1,55 @@
+package dev.v3ktor.vacancymanagementa.modules.company.entitys;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity( name = "job")
+public class JobEntity {
+
+    // Atributos
+    @Id @GeneratedValue( strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne() @NotNull
+    private CompanyEntity company;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    // Construtores
+    public JobEntity() { }
+    public JobEntity(UUID id, String description, String benefits, String level, CompanyEntity company, LocalDateTime createdAt) {
+        this.id = id;
+        this.description = description;
+        this.benefits = benefits;
+        this.level = level;
+        this.company = company;
+        this.createdAt = createdAt;
+    }
+
+    // Getters & Setters
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getBenefits() { return benefits; }
+    public void setBenefits(String benefits) { this.benefits = benefits; }
+
+    public String getLevel() { return level; }
+    public void setLevel(String level) { this.level = level; }
+
+    public CompanyEntity getCompany() { return company; }
+    public void setCompany(@NotNull CompanyEntity company) { this.company = company; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
